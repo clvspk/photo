db769edd103965c2cd76eb7f31b2d1abc2b5efad
clvspk <516342079@qq.com>
2018-08-21 09:44:01 +0800

    优化

--- src/main/java/com/clvspk/photo/controller/UploadController.java
+++ src/main/java/com/clvspk/photo/controller/UploadController.java
@@ -12,7 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,6 +25,10 @@ public class UploadController {
     @Autowired
     private Config config;
 
+    private String basePath = null;
+
+    private Long overTime = null;
+
 
     @PostMapping
     public List<String> upload(@RequestParam("imgFile") MultipartFile[] multipartFiles) throws IOException {
@@ -30,8 +36,8 @@ public class UploadController {
             Result.fail("请求参数异常");
         }
         List<String> list = new ArrayList<>();
+        String basePath  = getBasePath();
         //图片基础路径
-        String basePath = getImgPath();
         for (MultipartFile multipartFile : multipartFiles) {
             String picName =
                     UUID.randomUUID().toString().replace("-", "")
@@ -46,33 +52,40 @@ public class UploadController {
         return list;
     }
 
-    private String getImgPath() {
-        String year = String.valueOf(LocalDateTime.now().getYear());
-        String month = String.valueOf(LocalDateTime.now().getMonth().getValue());
-        String day = String.valueOf(LocalDateTime.now().getDayOfMonth());
-        if (month.length() < 2) {
-            month = "0" + month;
-        }
-        if (month.length() < 2) {
-            day = "0" + day;
-        }
-        File file = new File(
-                config.getBasePath()
-                        + year + File.separator
-                        + month + File.separator
-                        + day);
-        if (!file.exists()) {
-            boolean createFileSuccess = file.mkdirs();
-            if (!createFileSuccess) {
-                Result.fail("文件目录创建失败");
-                System.out.println(file.getPath());
+    private String getBasePath() {
+        if (this.overTime == null || System.currentTimeMillis() > this.overTime){
+            synchronized (this){
+                String year = String.valueOf(LocalDateTime.now().getYear());
+                String month = String.valueOf(LocalDateTime.now().getMonth().getValue());
+                String day = String.valueOf(LocalDateTime.now().getDayOfMonth());
+                if (month.length() < 2) {
+                    month = "0" + month;
+                }
+                if (month.length() < 2) {
+                    day = "0" + day;
+                }
+                File file = new File(
+                        config.getBasePath()
+                                + year + File.separator
+                                + month + File.separator
+                                + day);
+                if (!file.exists()) {
+                    boolean createFileSuccess = file.mkdirs();
+                    if (!createFileSuccess) {
+                        Result.fail("文件目录创建失败");
+                        System.out.println(file.getPath());
+                    }
+                }
+                this.basePath = file.getPath();
+                this.overTime = Date.from(LocalDateTime.now().plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0).atZone(ZoneId.systemDefault()).toInstant()).getTime();
             }
         }
-        return file.getPath();
+        return this.basePath;
     }
 
     private String getHttpUrl(String filePath) {
         return config.getLocalhost() + filePath.replace(config.getBasePath(), "").replace("\\", "/");
     }
+
 
 }
